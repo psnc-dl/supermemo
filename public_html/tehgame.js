b5beddfb54c526d47dd411a1a9ce8608d474cba4
witek1 <witeknosorowski@wp.pl>
2014-08-04 14:17:47 +0200

    Card matching system added.

--- public_html/tehgame.js
+++ public_html/tehgame.js
@@ -2,6 +2,10 @@ var main = function() {
 
     var images = ["images/kitten1.jpg", "images/kitten1.jpg", "images/kitten2.jpg", 
         "images/kitten2.jpg", "images/kitten3.jpg", "images/kitten3.jpg"];
+    var clickCount = 0;
+    var points = 0;
+    var matchCard = [];
+    var matchIndex = 0;
 
     var randomize = function() {
         var x = Math.floor(Math.random() * 6);
@@ -22,31 +26,49 @@ var main = function() {
     document.getElementById("card5").src = images[4];
     document.getElementById("card6").src = images[5];
     
-    $(card1).click(function() { $(this).css("opacity","1"); });
-    $(card2).click(function() { $(this).css("opacity","1"); });
-    $(card3).click(function() { $(this).css("opacity","1"); });
-    $(card4).click(function() { $(this).css("opacity","1"); });
-    $(card5).click(function() { $(this).css("opacity","1"); });
-    $(card6).click(function() { $(this).css("opacity","1"); });
-        
-    var clickCount = 0;
+    $(card1).click(function() { $(this).css("opacity", "1"); });
+    $(card2).click(function() { $(this).css("opacity", "1"); });
+    $(card3).click(function() { $(this).css("opacity", "1"); });
+    $(card4).click(function() { $(this).css("opacity", "1"); });
+    $(card5).click(function() { $(this).css("opacity", "1"); });
+    $(card6).click(function() { $(this).css("opacity", "1"); });
     
-    $(".card").click(function() {
+    $(".card").click( function(event) {
         
         ++clickCount;
         $(test).text(clickCount);
         
+        var matchId = event.target.id;
+        
+        matchCard[matchIndex] = matchId;
+        $(test1).text(matchCard);
+        
         if (clickCount === 2) {
-            $(test).text("Juz 2");
-            setTimeout(function() { 
-                $("img").css("opacity", "0");
-                $(test).text(clickCount);
-            }, 800);
+            $(test).text("Już 2");
+            
+            if ( document.getElementById(matchCard[0]).src === document.getElementById(matchCard[1]).src ) {
+                ++points;
+                $(test2).text(points);
+                document.getElementById(matchCard[0]).className = "";
+                document.getElementById(matchCard[1]).className = "";
+                if (points === 3) { $(test3).text("GZ you won!!!") ;}
+            } else {
+                setTimeout(function() { 
+                    $(".covered").css("opacity", "0");
+                    $(test).text(clickCount);
+                }, 800); 
+            }
            clickCount = 0;
         }
-          
+        matchIndex++;
+        
+        if (matchCard.length === 2) {
+            matchCard.splice(0, 2);
+            matchIndex = 0;
+        }
+        
     });
-
+    
 };
 
 $(document).ready(main);
