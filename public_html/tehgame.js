b212073c6f9da0c873a83e86f371ab1c1f525f5a
witek1 <witeknosorowski@wp.pl>
2014-08-05 13:27:25 +0200

    Improved gameboard with better layout, added some dynamic functions

--- public_html/tehgame.js
+++ public_html/tehgame.js
@@ -6,9 +6,10 @@ var main = function() {
     var points = 0;
     var matchCard = [];
     var matchIndex = 0;
+    var cardCount = 6;
 
     var randomize = function() {
-        var x = Math.floor(Math.random() * 6);
+        var x = Math.floor(Math.random() * cardCount);
         var imgTemp = [];
         imgTemp[0] = images[x];
         images.splice(x, 1);
@@ -18,34 +19,27 @@ var main = function() {
     for (i = 0; i < 7; i++) {
         randomize();
     }
+    
+    for (i = 0; i < cardCount; i++) {
+        document.getElementById("card" + (i + 1).toString()).src = images[i];
+    }
+    
+    for (i = 0; i < cardCount; i++) {
+        $("#card" + (i + 1).toString()).click(function() { $(this).css("opacity", "1"); });
+    }
+    
 
-    document.getElementById("card1").src = images[0];
-    document.getElementById("card2").src = images[1];
-    document.getElementById("card3").src = images[2];
-    document.getElementById("card4").src = images[3];
-    document.getElementById("card5").src = images[4];
-    document.getElementById("card6").src = images[5];
-    
-    $(card1).click(function() { $(this).css("opacity", "1"); });
-    $(card2).click(function() { $(this).css("opacity", "1"); });
-    $(card3).click(function() { $(this).css("opacity", "1"); });
-    $(card4).click(function() { $(this).css("opacity", "1"); });
-    $(card5).click(function() { $(this).css("opacity", "1"); });
-    $(card6).click(function() { $(this).css("opacity", "1"); });
-    
     $(".card").click( function(event) {
         
         ++clickCount;
         $(test).text(clickCount);
-        
-        var matchId = event.target.id;
-        
+        var matchId = event.target.id; 
         matchCard[matchIndex] = matchId;
         $(test1).text(matchCard);
         
         if (clickCount === 2) {
-            $(test).text("Już 2");
             
+            $(test).text("Już 2");
             if ( document.getElementById(matchCard[0]).src === document.getElementById(matchCard[1]).src ) {
                 ++points;
                 $(test2).text(points);
